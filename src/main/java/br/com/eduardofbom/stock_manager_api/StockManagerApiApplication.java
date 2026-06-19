1c3fbc570c54da0f196c0b018ace2482ddbfa844
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-06-19 12:31:14 -0300

    chore(dependencias): setup inicial do projeto com spring boot, jpa, flyway e swagger

---
+++ src/main/java/br/com/eduardofbom/stock_manager_api/StockManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardofbom.stock_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardofbom/stock_manager_api/StockManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardofbom.stock_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
