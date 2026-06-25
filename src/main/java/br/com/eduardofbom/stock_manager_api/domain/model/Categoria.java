e22b05146d7d2fbc98c4c4aac2cc903fa8632ff4
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-06-25 20:55:51 -0300

    feat(domain): mapeia entidades JPA para categoria e fornecedor

---
+++ src/main/java/br/com/eduardofbom/stock_manager_api/domain/model/Categoria.java
@@ -0,0 +1,38 @@
+package br.com.eduardofbom.stock_manager_api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    protected Categoria() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+
+        Categoria outra = (Categoria) o;
+        return id != null && id.equals(outra.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/br/com/eduardofbom/stock_manager_api/domain/model/Fornecedor.java
@@ -0,0 +1,41 @@
+package br.com.eduardofbom.stock_manager_api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "fornecedores")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column(name = "documento_fiscal", length = 30)
+    private String documentoFiscal;
+
+    protected Fornecedor() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fornecedor)) return false;
+
+        Fornecedor outra = (Fornecedor) o;
+        return id != null && id.equals(outra.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
