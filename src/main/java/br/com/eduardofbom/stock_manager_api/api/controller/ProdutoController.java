8b1d249fb37f85193f30fc538e1ef367db98165a
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-07-02 15:31:13 -0300

    feat(produto): implementa endpoint de criacao de produtos

--- src/main/java/br/com/eduardofbom/stock_manager_api/api/controller/ProdutoController.java
+++ src/main/java/br/com/eduardofbom/stock_manager_api/api/controller/ProdutoController.java
@@ -1,11 +1,12 @@
 package br.com.eduardofbom.stock_manager_api.api.controller;
 
+import br.com.eduardofbom.stock_manager_api.api.dto.ProdutoRequestDTO;
 import br.com.eduardofbom.stock_manager_api.api.dto.ProdutoResponseDTO;
 import br.com.eduardofbom.stock_manager_api.domain.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +24,11 @@ public class ProdutoController {
     public ResponseEntity<List<ProdutoResponseDTO>> listar() {
         List<ProdutoResponseDTO> produtos = produtoService.listarTodos();
         return ResponseEntity.ok(produtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> criar (@RequestBody @Valid ProdutoRequestDTO produtoRequestDTO) {
+        ProdutoResponseDTO produtoCriado = produtoService.criar(produtoRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoCriado);
     }
 }
--- src/main/java/br/com/eduardofbom/stock_manager_api/domain/service/ProdutoService.java
+++ src/main/java/br/com/eduardofbom/stock_manager_api/domain/service/ProdutoService.java
@@ -1,6 +1,10 @@
 package br.com.eduardofbom.stock_manager_api.domain.service;
 
+import br.com.eduardofbom.stock_manager_api.api.dto.ProdutoRequestDTO;
 import br.com.eduardofbom.stock_manager_api.api.dto.ProdutoResponseDTO;
+import br.com.eduardofbom.stock_manager_api.domain.model.Categoria;
+import br.com.eduardofbom.stock_manager_api.domain.model.Produto;
+import br.com.eduardofbom.stock_manager_api.domain.repository.ICategoriaRepository;
 import br.com.eduardofbom.stock_manager_api.domain.repository.IProdutoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +15,11 @@ import java.util.List;
 public class ProdutoService {
 
     private final IProdutoRepository produtoRepository;
+    private final ICategoriaRepository categoriaRepository;
 
-    public ProdutoService(IProdutoRepository produtoRepository) {
+    public ProdutoService(IProdutoRepository produtoRepository, ICategoriaRepository categoriaRepository) {
         this.produtoRepository = produtoRepository;
+        this.categoriaRepository = categoriaRepository;
     }
 
     @Transactional(readOnly = true)
@@ -22,5 +28,27 @@ public class ProdutoService {
                 .stream()
                 .map(ProdutoResponseDTO::new)
                 .toList();
+    }
+
+    @Transactional
+    public ProdutoResponseDTO criar(ProdutoRequestDTO produtoRequestDTO) {
+        Categoria categoria = categoriaRepository.findById(produtoRequestDTO.categoriaId())
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Não é possível cadastrar o produto. Categoria não encontrada com o ID: "
+                        + produtoRequestDTO.categoriaId()
+                ));
+
+        Produto novoProduto = new Produto(
+                produtoRequestDTO.nome(),
+                categoria,
+                produtoRequestDTO.unidMedida(),
+                produtoRequestDTO.precoVenda()
+        );
+        novoProduto.setCodBarras(produtoRequestDTO.codBarras());
+        novoProduto.setEstoqMin(produtoRequestDTO.estoqMin());
+
+        Produto produtoSalvo = produtoRepository.save(novoProduto);
+
+        return new ProdutoResponseDTO(produtoSalvo);
     }
 }
